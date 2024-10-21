b5f5d24225b8a866e1305de8d0fb9cf1cb90ef7e
habibapanna <habibapanna49@gmail.com>
2024-10-22 00:32:04 +0600

    added a blogs.jsx file

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
 
 
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark}) => {
 const [blogs, setBlogs] = useState([]);
 
 useEffect(() =>{
@@ -15,7 +15,8 @@ useEffect(() =>{
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                blogs.map(blog => <Blog key={blog.id} blog={blog}
+                    handleAddToBookmark={handleAddToBookmark}></Blog>)
             }
         </div>
     );
